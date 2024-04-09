ae4fd9037efd9a279c050c9e013dc0d8c5c6b92c
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-04-09 07:59:45 +0300

    add ws models

---
+++ persiktunes/models/ws.py
@@ -0,0 +1,92 @@
+from pydantic import BaseModel
+from typing import Optional, Any, Literal, Union, List
+from models.base import LavalinkTrack, LavalinkExceptionResponse
+
+"""
+LAVALINK WS MODELS
+"""
+
+
+class BaseWebsocketRequest(BaseModel):
+    op: Literal["ready", "playerUpdate", "event", "stats"]
+
+
+class PlayerState(BaseModel):
+    time: int
+    position: int
+    connected: bool
+    ping: int
+
+
+class Memory(BaseModel):
+    free: int
+    used: int
+    allocated: int
+    reservable: int
+
+
+class Cpu(BaseModel):
+    cores: int
+    systemLoad: float
+    lavalinkLoad: float
+
+
+class FrameStats(BaseModel):
+    sent: int
+    nulled: int
+    deflict: Optional[int] = None
+
+
+class ReadyOP(BaseWebsocketRequest):
+    resumed: bool
+    sessionId: str
+
+
+class PlayerUpdateOP(BaseWebsocketRequest):
+    guildId: str
+    state: PlayerState
+
+
+class StatsOP(BaseWebsocketRequest):
+    players: int
+    playingPlayers: int
+    uptime: int
+    memory: Memory
+    cpu: Cpu
+    frameStats: Optional[FrameStats] = None
+
+
+class EventOP(BaseWebsocketRequest):
+    type: Literal[
+        "TrackStartEvent",
+        "TrackEndEvent",
+        "TrackStuckEvent",
+        "TrackExceptionEvent",
+        "WebSocketClosedEvent",
+    ]
+    guildId: str
+
+
+class TrackStartEvent(EventOP):
+    track: LavalinkTrack
+
+
+class TrackEndEvent(EventOP):
+    track: LavalinkTrack
+    reason: Literal["finished", "loadFailed", "stopped", "replaced", "cleanup"]
+
+
+class TrackExceptionEvent(EventOP):
+    track: LavalinkTrack
+    exception: LavalinkExceptionResponse
+
+
+class TrackStuckEvent(EventOP):
+    track: LavalinkTrack
+    threshold: int
+
+
+class WebSocketClosedEvent(EventOP):
+    code: int
+    reason: str
+    byRemote: bool
