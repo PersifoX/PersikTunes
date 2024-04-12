4b0deb05acacd758f9375e8de9bc82e379686a25
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-04-12 19:56:20 +0300

    add player base

--- persiktunes/models/restapi.py
+++ persiktunes/models/restapi.py
@@ -82,3 +82,117 @@ class LavalinkTrackLoadingResponse(BaseModel):
             LavalinkExceptionResponse,
         ]
     ] = {}
+
+
+class LavalinkTrackDecodeResponse(LavalinkTrack):
+    """Response model is analog to LavalinkTrack."""
+
+
+class LavalinkTrackDecodeMultiplyResponse(BaseModel):
+    """Base lavalink track decode multiply response model."""
+
+    tracks: List[LavalinkTrack]
+
+
+"""
+LAVALINK PLAYER ALI MODELS
+"""
+
+
+class VoiceState(BaseModel):
+    token: str
+    endpoint: str
+    sessionId: str
+
+
+class PlayerState(BaseModel):
+    time: int
+    position: int
+    connected: bool
+    ping: int
+
+
+"FILTERS MODELS"
+
+
+class EqualizerBand(BaseModel):
+    band: int
+    gain: float
+
+
+class Karaoke(BaseModel):
+    level: Optional[float] = None
+    monoLevel: Optional[float] = None
+    filterBand: Optional[float] = None
+    filterWidth: Optional[float] = None
+
+
+class Timescale(BaseModel):
+    speed: Optional[float] = None
+    pitch: Optional[float] = None
+    rate: Optional[float] = None
+
+
+class Tremolo(BaseModel):
+    frequency: Optional[float] = None
+    depth: Optional[float] = None
+
+
+class Vibrato(BaseModel):
+    frequency: Optional[float] = None
+    depth: Optional[float] = None
+
+
+class Rotation(BaseModel):
+    rotationHz: Optional[float] = None
+
+
+class Distortion(BaseModel):
+    sinOffset: Optional[float] = None
+    sinScale: Optional[float] = None
+    cosOffset: Optional[float] = None
+    cosScale: Optional[float] = None
+    tanOffset: Optional[float] = None
+    tanScale: Optional[float] = None
+    offset: Optional[float] = None
+    scale: Optional[float] = None
+
+
+class ChannelMix(BaseModel):
+    leftToLeft: Optional[float] = None
+    leftToRight: Optional[float] = None
+    rightToLeft: Optional[float] = None
+    rightToRight: Optional[float] = None
+
+
+class LowPass(BaseModel):
+    smoothing: Optional[float] = None
+
+
+class Filters(BaseModel):
+    volume: Optional[float] = 1.0
+    equalizer: Optional[List[EqualizerBand]] = None
+    karaoke: Optional[Karaoke] = None
+    timescale: Optional[Timescale] = None
+    tremolo: Optional[Tremolo] = None
+    vibrato: Optional[Vibrato] = None
+    rotation: Optional[Rotation] = None
+    distortion: Optional[Distortion] = None
+    channelMix: Optional[ChannelMix] = None
+    lowPass: Optional[LowPass] = None
+    pluginFilters: Optional[Any] = None
+
+
+"""
+LAVALINK PLAYER API
+"""
+
+
+class LavalinkPlayer(BaseModel):
+    guildId: int
+    track: Optional[LavalinkTrack] = None
+    volume: int
+    paused: bool
+    state: PlayerState
+    voice: VoiceState
+    filters: Filters
