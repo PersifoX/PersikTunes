266809b933033a7de19404a09bad9ab2c3d8d93f
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2024-05-26 12:26:45 +0300

    new search models

--- persiktunes/models/search.py
+++ persiktunes/models/search.py
@@ -1,5 +1,29 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseModel
 
 from .restapi import Playlist, Track
+
+
+class Album(Playlist):
+    pass
+
+
+class Artist(BaseModel):
+    name: str
+    id: str
+    tracks: List[Track] = []
+    albums: Optional[List[Album]] = None
+    singles: Optional[List[Track]] = None
+    description: Optional[str] = None
+    thumbnail: Optional[str] = None
+
+
+class Mood(BaseModel):
+    title: str
+    params: str
+
+
+class Browse(BaseModel):
+    title: str
+    contents: List[Union[Playlist, Track, Playlist, Album, Artist, Mood]]
